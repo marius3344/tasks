44b251e1b60c1bfeb913d97a3ad30f28d5bc25ae
marius.simkunas.flgroup <marius3344@gmail.com>
2022-04-20 21:32:49 +0300

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AssignmentsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +23,9 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard',[AssignmentsController::class, 'index'])->name('dashboard');
+    Route::get('/assignment',[AssignmentsController::class, 'add']);
 });
+
+// To Do: Assignments index, add, create, edit, update routes
+
